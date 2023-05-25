b933b2e53b8b711af2a3f45fcaa23845d5b42ab1
Sergio Moubayed <sergio.moubayed@epita.fr>
2023-05-25 16:30:04 +0200

    feat(image) : added templates for the load
    Signed-off-by: Sergio Moubayed <sergio.moubayed@epita.fr>

--- cpu-sources/main.cc
+++ cpu-sources/main.cc
@@ -1,30 +1,54 @@
 #include <iostream>
 
-#include "utility/imagePng.hh"
+#include "utility/image.hh"
+#include <fstream>
+// #include "utility/imagePng.hh"
 
+
+void to_ppm(shared_image image, std::string name)
+{
+    std::ofstream file(name);
+    file << "P3" << std::endl;
+    file << image->get_width() << " " << image->get_height() << std::endl;
+    file << "255" << std::endl;
+
+    for (size_t y = 0; y < image->get_height(); y++)
+    {
+        for (size_t x = 0; x < image->get_width(); x++)
+        {
+            file << int((*image)(x, y)[0]) << " "
+                 << int((*image)(x, y)[1]) << " "
+                 << int((*image)(x, y)[2]) << " ";
+        }
+        file << std::endl;
+    }
+}
 
 int main()
 {
     std::string datasetPath = std::string(DATASET_DIR) + "/frames";
 
-    ImagePng* image = ImagePng::load((datasetPath + "/1.png").c_str());
+    shared_image image =
+        load_png((datasetPath + "/1.png").c_str());
 
-    std::cout << "Width: " << image->getWidth() << std::endl;
-    std::cout << "Height: " << image->getHeight() << std::endl;
+    std::cout << "Image width: " << image->get_width() << std::endl;
+    std::cout << "Image height: " << image->get_height() << std::endl;
+    std::cout << "test: " << int((*image)(1, 1)[0]) << std::endl;
 
-    for (size_t y = 0; y < image->getHeight(); y++) {
-        for (size_t x = 0; x < image->getWidth(); x++) {
-            png_byte* pixel = &(image->getRowPointers()[y][x * 3]);
-            std::cout << "R: " << (int)pixel[0] << " ";
-            std::cout << "G: " << (int)pixel[1] << " ";
-            std::cout << "B: " << (int)pixel[2] << " ";
-        }
-        std::cout << std::endl;
-    }
+    // Print all pixels in the image
+    // for (size_t y = 0; y < image->get_height(); y++)
+    // {
+    //     for (size_t x = 0; x < image->get_width(); x++)
+    //     {
+    //         std::cout << "(" << int((*image)(x, y)[0]) << ", "
+    //                   << int((*image)(x, y)[1]) << ", "
+    //                   << int((*image)(x, y)[2]) << ") ";
+    //     }
+    //     std::cout << std::endl;
+    // }
 
-    ImagePng::save("test.png", image);
+    to_ppm(image, "test.ppm");
 
-    delete image;
 
     return EXIT_SUCCESS;
 }

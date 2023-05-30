946224e5c25a6831f23bc08866c0564d94e8ba8f
david.chemaly <davidchemaly1@gmail.com>
2023-05-30 21:40:47 +0200

    fix(Color) : fixed color similarity measures

--- cpu-sources/main.cpp
+++ cpu-sources/main.cpp
@@ -11,8 +11,8 @@
 int main()
 {
     std::string datasetPath = std::string(DATASET_DIR) + "/frames";
-    std::string frame1 = datasetPath + "/1.png";
-    std::string frame2 = datasetPath + "/2.png";
+    std::string frame1 = datasetPath + "/3.png";
+    std::string frame2 = datasetPath + "/4.png";
 
     shared_image image1 = load_png(frame1);
     shared_image image2 = load_png(frame2);
@@ -20,22 +20,31 @@ int main()
     std::cout << "Image 1: " << image1->get_width() << "x" << image1->get_height() << std::endl;
     std::cout << "Image 2: " << image2->get_width() << "x" << image2->get_height() << std::endl;
 
+    shared_image image1_YCrCb = std::make_shared<Image<Pixel>>(image1->get_width(), image1->get_height());
+    shared_image image2_YCrCb = std::make_shared<Image<Pixel>>(image2->get_width(), image2->get_height());
+
     shared_image resultImage = std::make_shared<Image<Pixel>>(image1->get_width(), image1->get_height());
+
+    float* weights = new float[3];
+    weights[0] = 0.1f;
+    weights[1] = 0.3f;
+    weights[2] = 0.6f;
 
     for (int y = 0; y < image1->get_height(); y++)
     {
         for (int x = 0; x < image1->get_width(); x++)
         {
             // Color
-            shared_image image1_YCrCb = std::make_shared<Image<Pixel>>(image1->get_width(), image1->get_height());
-            shared_image image2_YCrCb = std::make_shared<Image<Pixel>>(image2->get_width(), image2->get_height());
-            float* yCrCb1 = RGBtoYCrCB(image1->get(x, y));
-            float* yCrCb2 = RGBtoYCrCB(image2->get(x, y));
-            Pixel yCrCb1Pixel = { yCrCb1[0], yCrCb1[1], yCrCb1[2] };
-            Pixel yCrCb2Pixel = { yCrCb2[0], yCrCb2[1], yCrCb2[2] };
-            image1_YCrCb->set(x, y, yCrCb1Pixel);
-            image2_YCrCb->set(x, y, yCrCb2Pixel);
-            float* colorComponents = getSimilarityMeasures(image1_YCrCb->get(x, y), image2_YCrCb->get(x, y));
+            // float* yCrCb1 = RGBtoYCrCB(image1->get(x, y));
+            // float* yCrCb2 = RGBtoYCrCB(image2->get(x, y));
+
+            // Pixel yCrCb1Pixel = { yCrCb1[0], yCrCb1[1], yCrCb1[2] };
+            // Pixel yCrCb2Pixel = { yCrCb2[0], yCrCb2[1], yCrCb2[2] };
+            // image1_YCrCb->set(x, y, yCrCb1Pixel);
+            // image2_YCrCb->set(x, y, yCrCb2Pixel);
+            // float* colorComponents = getSimilarityMeasures(image1_YCrCb->get(x, y), image2_YCrCb->get(x, y));
+
+            float* colorComponents = getSimilarityMeasures(image1->get(x, y), image2->get(x, y));
 
             // Texture
             uint8_t vector1 = getVector(image1, x, y);
@@ -45,20 +54,13 @@ int main()
 
             // Choquet integral
             float* vector3 = new float[3];
-            vector3[0] = colorComponents[0];
-            vector3[1] = colorComponents[1];
+            vector3[0] = colorComponents[1];
+            vector3[1] = colorComponents[2];
             vector3[2] = textureComponent;
 
             std::sort(vector3, vector3 + 3);
 
-            float* weights = new float[3];
-            weights[0] = 0.1f;
-            weights[1] = 0.3f;
-            weights[2] = 0.6f;
-
             float result = vector3[0] * weights[0] + vector3[1] * weights[1] + vector3[2] * weights[2];
-
-            delete[] vector3;
 
             if (result < 0.67f)
             {
@@ -72,8 +74,12 @@ int main()
                 // Add a black pixel to the result image
                 resultImage->set(x, y, { 0, 0, 0 });
             }
+
+            delete[] vector3;
         }
     }
+
+    delete[] weights;
 
     save_png("result.png", resultImage);
 
--- cpu-sources/sources/color/similarityMeasuresC.cpp
+++ cpu-sources/sources/color/similarityMeasuresC.cpp
@@ -1,12 +1,42 @@
 #include "color/similarityMeasuresC.h"
 
+uint8_t min(char a, char b)
+{
+    uint8_t res = a <= b ? a : b;
+    return res;
+}
+
+uint8_t max(char a, char b)
+{
+    uint8_t res = a >= b ? a : b;
+    return res;
+}
+
 float* getSimilarityMeasures(Pixel pixelB, Pixel pixelF)
 {
     float* similarityMeasures = new float[3];
 
-    float SY = std::min(pixelB[0], pixelF[0]) / std::max(pixelB[0], pixelF[0]);
-    float SCr = std::min(pixelB[1], pixelF[1]) / std::max(pixelB[1], pixelF[1]);
-    float SCb = std::min(pixelB[2], pixelF[2]) / std::max(pixelB[2], pixelF[2]);
+    if (pixelB[0] == 0 && pixelF[0] == 0)
+    {
+        pixelB[0] = 1;
+        pixelF[0] = 1;
+    }
+
+    if (pixelB[1] == 0 && pixelF[1] == 0)
+    {
+        pixelB[1] = 1;
+        pixelF[1] = 1;
+    }
+
+    if (pixelB[2] == 0 && pixelF[2] == 0)
+    {
+        pixelB[2] = 1;
+        pixelF[2] = 1;
+    }
+
+    float SY = (float)min(pixelB[0], pixelF[0]) / (float)max(pixelB[0], pixelF[0]);
+    float SCr = (float)min(pixelB[1], pixelF[1]) / (float)max(pixelB[1], pixelF[1]);
+    float SCb = (float)min(pixelB[2], pixelF[2]) / (float)max(pixelB[2], pixelF[2]);
 
     similarityMeasures[0] = SY;
     similarityMeasures[1] = SCr;
